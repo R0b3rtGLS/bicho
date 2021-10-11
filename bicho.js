573e3e8657452325da58e85f25ba2e2816a690d1
Robert Rivero <rivero.robert@gmail.com>
2021-10-11 15:52:35 +0200

    test tap with timer

--- bicho.js
+++ bicho.js
@@ -1,1 +1,14 @@
-Android.sendTap(500, 500);
+//Android.startApp("com.panzerdog.tacticool");
+
+var timer = new Timer();
+timer.start();
+
+
+
+var point = new Point(500, 500);
+Android.sendTap(point);
+
+
+if(Timer.getElapsedTime() >= 15000){
+    Stats.show("General", "Progress", "10%");
+}
